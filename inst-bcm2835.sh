f14082007115aff66ea96d4ab1f9a6393c574134
Geoff Rehmet <geoff@rehmet.alt.za>
2022-01-19 19:08:29 +1100

    Update libbcm2835 to version 1.71

--- inst-bcm2835.sh
+++ inst-bcm2835.sh
@@ -1,8 +1,8 @@
 if [ ! -d bcm2835 ]  ; then  mkdir bcm2835 ;fi
 cd bcm2835
-wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz
-tar xvfz bcm2835-1.60.tar.gz
-cd bcm2835-1.60
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.71.tar.gz
+tar xvfz bcm2835-1.71.tar.gz
+cd bcm2835-1.71
 ./configure
 make
 sudo make install
